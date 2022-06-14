4d853d3949ebffc333633e4c4baa9e9a5490010f
Bruno Campos <brunofdcampos@hotmail.com>
2022-06-14 23:05:31 +0100

    feat: from the 'the_movies_dataset' files, we got a simple way of getting the top x movies with a weighted score

--- beginner_tutorial_recommender_systems_python_datacamp/simple_recommenders/top-movies.py
+++ beginner_tutorial_recommender_systems_python_datacamp/simple_recommenders/top-movies.py
@@ -1,1 +1,31 @@
 import pandas as pd
+
+metadata = pd.read_csv('~/Documents/Documents - MACQ199NQ496F/machine_learning/beginner_tutorial_recommender_systems_python_datacamp/the_movies_dataset/movies_metadata.csv', low_memory=False)
+
+print(metadata.head(3))
+
+C = metadata['vote_average'].mean()
+
+print( C )
+
+m = metadata['vote_count'].quantile(0.90)
+
+print(m)
+
+q_movies = metadata.copy().loc[metadata['vote_count'] >= m ]
+
+print(q_movies.shape)
+
+print(metadata.shape)
+
+def weighted_rating(x, m=m, C=C):
+    v = x['vote_count']
+    R = x['vote_average']
+
+    return (v / (v + m) ) * R + ( v / (v + m) ) * C
+
+q_movies['Score'] = q_movies.apply(weighted_rating, axis=1)
+
+q_movies = q_movies.sort_values('Score', ascending=False)
+
+print(q_movies[['title', 'vote_count', 'vote_average', 'Score']].head(20))
